d2cbaca7c1311f4a7c03426e5fa85339be807ce9
renantescaro <renantescaro@yahoo.com.br>
2021-02-09 18:15:01 -0300

    renomeando arquivos

--- environ.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Environ():
-    def set_variaveis(self):
-        os.environ['API_USER'] = 'username'
-        os.environ['API_PASSWORD'] = 'secret'
-
-    def get_variaveis(self):
-        pass
