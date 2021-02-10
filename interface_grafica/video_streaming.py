2f672cae0d21c0ea3fe5538967b3d188dcaadf9e
renantescaro <renantescaro@yahoo.com.br>
2021-02-09 21:33:10 -0300

    implementando o streaming de video

--- interface_grafica/video_streaming.py
+++ interface_grafica/video_streaming.py
@@ -1,24 +1,26 @@
 import tkinter as tk
+from PIL import Image, ImageTk
+import cv2
 
 class VideoStreaming(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.grid()
-        self.criar_componentes()
+    def __init__(self):
+        self.master = tk.Tk()
+        self.master.geometry('800x600')
+        super().__init__(self.master)
+        self.camera = cv2.VideoCapture('http://192.168.0.200:8080/?action=stream')
+        app         = tk.Frame(self.master, bg="white")
+        app.grid()
+        self.master = tk.Label(app)
+        self.master.grid()
+        self.video_componente()
+        app.mainloop()
 
 
-    def criar_componentes(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there.pack(side="top")
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
-
-
-def iniciar():
-    tkinter = tk.Tk()
-    tkinter.geometry('500x300')
-    app = VideoStreaming(master=tkinter)
-    app.mainloop()
+    def video_componente(self):
+        conectado, imagem_cv2 = self.camera.read()
+        resize_img = cv2.resize(imagem_cv2, (800, 600))
+        img        = Image.fromarray(resize_img)
+        imgtk      = ImageTk.PhotoImage(image=img)
+        self.master.imgtk = imgtk
+        self.master.configure(image=imgtk)
+        self.master.after(1, self.video_componente)
